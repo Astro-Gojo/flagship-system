60cf4ed0b0d44d66ae7f7374bde2654d122558df
Goku <gokulnair1704@gmail.com>
2025-12-23 20:57:52 +0530

    Add files via upload
    Day 2 — Simulated sensor module

--- sensor_simulation.py
+++ sensor_simulation.py
@@ -1,1 +1,13 @@
+import random
 
+# Simulate sensor input
+sensor_value = random.randint(0, 100)  # light sensor example
+
+# Processing (simple rule)
+if sensor_value < 50:
+    output = "Lights ON"
+else:
+    output = "Lights OFF"
+
+# Output
+print(f"Sensor value: {sensor_value}, Action: {output}")
